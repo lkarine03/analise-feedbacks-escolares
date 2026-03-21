db1405db275ddcc27e8c6027a899af23ce05569e
lkarine03 <karinelana03@gmail.com>
2026-03-21 15:40:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const busca = document.getElementById("busca");
 const itens = document.querySelectorAll(".feedback-list li");
+const mensagem = document.getElementById("mensagem");
 
 busca.addEventListener("input", () => {
     const texto = busca.value.toLowerCase().trim();
+    let encontrou = false;
 
     itens.forEach(item => {
         const conteudo = item.textContent.toLowerCase();
 
-        item.style.display = conteudo.includes(texto) ? "" : "none";
+        if (conteudo.includes(texto)) {
+            item.style.display = "";
+            encontrou = true;
+        } else {
+            item.style.display = "none";
+        }
     });
+    mensagem.style.display = encontrou ? "none" : "block";
 });
